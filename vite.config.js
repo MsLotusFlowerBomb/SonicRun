8a0040499c6694cc54cb5f4af4047427d10513ce
Kgaugelo Kay <165329719+MsLotusFlowerBomb@users.noreply.github.com>
2024-12-29 11:23:50 +0200

    second commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // remove if you're not using react
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/SonicRun",
+  
+});
